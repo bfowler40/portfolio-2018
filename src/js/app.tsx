f7af22fcc0b628474ee7ad323fbf39a0846f4dce
Ben Fowler <bfowler40@gmail.com>
2017-12-24 16:01:50 +1100

    add webfontloader

--- src/js/app.tsx
+++ src/js/app.tsx
@@ -1,8 +1,16 @@
-import * as React from 'react';
+import * as React    from 'react';
 import * as ReactDOM from 'react-dom';
+import * as WebFont  from 'webfontloader';
+import { Root }      from './components/root';
 
-import { Root } from './components/root';
+// Load the font from google
+WebFont.load({
+	google: {
+		families: ['Fira Mono'],
+	},
+});
 
+// The React App
 ReactDOM.render(
 	<Root />,
 	document.getElementById('app')
